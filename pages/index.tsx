dbe067e9c6c71225a2dd0c15c782a13d86045852
amends <amends@protonmail.com>
2021-10-01 16:12:17 -0700

    add BUSDStax

--- pages/index.tsx
+++ pages/index.tsx
@@ -107,7 +107,7 @@ function Home() {
           <Button colorScheme="gray"><Account triedToEagerConnect={triedToEagerConnect} /></Button>
           <Heading px={10} size="md" as="a" href="https://cakestax.money" color="white">Looking to bake CAKE? Click here for CakeStax.</Heading>
           <Heading px={10} size="md" as="a" href="https://ziggystax.money" color="white">Looking to mine POTS? Click here for ZiggyStax.</Heading>
-
+          <Heading px={10} size="md" as="a" href="https://busdstax.money" color="white">Looking to print BUSD? Click here for BUSDStax.</Heading>
           </VStack>
           </Center>
           <SimpleGrid column={5} spacing={5} justifyItems="center">
